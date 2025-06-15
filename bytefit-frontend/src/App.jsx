a1c638429c239bac29893f4ae8b86ccdbe44addf
rahul2004a <kumar.rahul.2017354@gmail.com>
2025-06-16 00:11:15 +0530

    Activity form and Activity List component

--- bytefit-frontend/src/App.jsx
+++ bytefit-frontend/src/App.jsx
@@ -5,6 +5,16 @@ import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "react-oauth2-code-pkce";
 import { useDispatch } from 'react-redux';
 import { setCredentials } from './store/authSlice';
+import ActivityDetail from './components/ActivityDetail';
+import ActivityForm from './components/ActivityForm';
+import ActivityList from './components/ActivityList';
+
+const ActvitiesPage = () => {
+  return (<Box sx={{ p: 2, border: '1px dashed grey' }}>
+    <ActivityForm onActivitiesAdded={() => window.location.reload()} />
+    <ActivityList />
+  </Box>);
+}
 
 function App() {
   const { token, tokenData, logIn, logOut, isAuthenticated } = useContext(AuthContext);
@@ -44,10 +54,17 @@ function App() {
           </Button>
         </Box>
       ) : (
-        <div>
-          <pre>{JSON.stringify(tokenData, null, 2)}</pre>
-          <pre>{JSON.stringify(token, null, 2)}</pre>
-        </div>
+        <Box sx={{ p: 2, border: '1px dashed grey' }}>
+          <Button variant="contained" color="secondary" onClick={logOut}>
+            Logout
+          </Button>
+          <Routes>
+            <Route path="/activities" element={<ActvitiesPage />} />
+            <Route path="/activities/:id" element={<ActivityDetail />} />
+
+            <Route path="/" element={token ? <Navigate to="/activities" replace /> : <div>Welcome! Please Login.</div>} />
+          </Routes>
+        </Box>
       )}
     </Router>
   )
