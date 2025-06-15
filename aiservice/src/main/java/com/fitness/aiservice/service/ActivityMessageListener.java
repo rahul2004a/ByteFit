f29e7fceb2a1552d2633c0dd20c973bb6cf2ae33
rahul2004a <kumar.rahul.2017354@gmail.com>
2025-06-16 01:05:16 +0530

    Building Activity Details Page

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -20,7 +20,8 @@ public class ActivityMessageListener {
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity) {
         log.info("Received activity for processing: {}", activity.getId());
-        log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
-
+//        log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
+        Recommendation recommendation = aiService.generateRecommendation(activity);
+        recommendationRepository.save(recommendation);
     }
 }
